72d30272e836eb8b97b7d9cbf7b307f86900c27a
rachelsigao <rachelsigao@gmail.com>
2026-01-21 15:57:02 -0500

    message

--- redis.sh
+++ redis.sh
@@ -1,39 +1,9 @@
 #!/bin/bash
 
-START_TIME=$(date +%s)
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/roboshop-logs" 
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD
+source ./common.sh
+app_name="redis"
 
-mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE
-
-#check for root access
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R ERROR: $N Please run this script with root access" | tee -a $LOG_FILE
-    exit 1 #give other than 0 upto 127
-else
-    echo "You are running with root access, Script is being installed" | tee -a $LOG_FILE
-fi
-
-#Validate function takes input as exit status, what command they tried to install
-VALIDATE ()
-    {
-    if [ $1 -eq 0 ]
-    then 
-        echo -e "$2 is $G SUCCESS $N" | tee -a $LOG_FILE
-    else 
-        echo -e "$2 is $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    fi
-    }
+check_root
 
 dnf module disable redis -y &>>$LOG_FILE
 VALIDATE $? "Disabling Redis"
@@ -45,13 +15,11 @@ dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Installing Redis"
 
 #i - permanent change, e - multiple expressions, s - substitute
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e 's/protected-mode yes/protected-mode no' /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e 's|protected-mode yes|protected-mode no|' /etc/redis/redis.conf
 VALIDATE $? "Editing Redis conf file for remote connections"
 
 systemctl enable redis 
 systemctl start redis 
 VALIDATE $? "Enabling and Starting Redis"
 
-END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+print_time
