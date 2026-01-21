4a9d1fa1c753616b04ed750a4123ef2451e7bf22
rachelsigao <rachelsigao@gmail.com>
2026-01-21 16:12:21 -0500

    message

--- redis.sh
+++ redis.sh
@@ -14,7 +14,7 @@ VALIDATE $? "Enabling Redis:7"
 dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Installing Redis"
 
-#i - permanent change, e - multiple expressions, s - substitute
+#i - permanent change, e - multiple expressions, s - substitute, Used | as delimiter for the second expression to avoid conflicts
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e 's|protected-mode yes|protected-mode no|' /etc/redis/redis.conf
 VALIDATE $? "Editing Redis conf file for remote connections"
 
